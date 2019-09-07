2de20b1185704948318c4a9b0aae1281487061c7
me <sniper96a@gmail.com>
2019-09-07 15:06:46 -0500

    forgot to commit build.zig

--- build.zig
+++ build.zig
@@ -10,18 +10,13 @@ pub fn build(b: *Builder) void {
     element_lib.setBuildMode(mode);
     element_lib.install();
 
-    var attribute_tests = b.addTest("src/html_attribute.zig");
-    attribute_tests.setBuildMode(mode);
-
     var debugexec = b.addExecutable("debugexec", "src/debugexec.zig");
     debugexec.setBuildMode(mode);
     debugexec.install();
 
+    var tests = b.addTest("src/html_element.zig");
+    tests.setBuildMode(mode);
+
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&attribute_tests.step);
-
-    var element_tests = b.addTest("src/html_element.zig");
-    element_tests.setBuildMode(mode);
-
-    test_step.dependOn(&element_tests.step);
+    test_step.dependOn(&tests.step);
 }
